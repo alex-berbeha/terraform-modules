9f6d976fd32b134e2797530b27c8e6429092ce45
berbeha.olexandr <berbeha.olexandr@gmail.com>
2021-08-10 13:09:33 +0300

    add VPC module

---
+++ AWS/modules/VPC/variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr" {
+  description = "Required, VPC cidr block"
+  default     = "10.0.0.0/20"
+  type = string
+}
+
+variable "vpc_tags" {
+  description = "VPC tags"
+  type = map
+}
