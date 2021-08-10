ab154623d9baf428ef26f833fd35ee63d3573108
berbeha.olexandr <berbeha.olexandr@gmail.com>
2021-08-10 13:12:14 +0300

    formatting with fmt

--- AWS/modules/VPC/variables.tf
+++ AWS/modules/VPC/variables.tf
@@ -1,10 +1,10 @@
 variable "vpc_cidr" {
   description = "Required, VPC cidr block"
   default     = "10.0.0.0/20"
-  type = string
+  type        = string
 }
 
 variable "vpc_tags" {
   description = "VPC tags"
-  type = map
+  type        = map
 }
